2a74f10124e6d9cdbfb764a4023af8ff79547057
sparktans <sparktansraise@gmail.com>
2017-11-12 10:23:22 +0530

    webapp commit

---
+++ campaign-web/src/main/webapp/4.chunk.js
@@ -0,0 +1,258 @@
+webpackJsonp([4],{
+
+/***/ "../../../../../src/app/layout/form/form-routing.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_router__ = __webpack_require__("../../../router/@angular/router.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__form_component__ = __webpack_require__("../../../../../src/app/layout/form/form.component.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return FormRoutingModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var routes = [
+    { path: '', component: __WEBPACK_IMPORTED_MODULE_2__form_component__["a" /* FormComponent */] }
+];
+var FormRoutingModule = (function () {
+    function FormRoutingModule() {
+    }
+    return FormRoutingModule;
+}());
+FormRoutingModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+        imports: [__WEBPACK_IMPORTED_MODULE_1__angular_router__["b" /* RouterModule */].forChild(routes)],
+        exports: [__WEBPACK_IMPORTED_MODULE_1__angular_router__["b" /* RouterModule */]]
+    })
+], FormRoutingModule);
+
+//# sourceMappingURL=form-routing.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/form/form.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "\n<div [@routerTransition]>\n  <!--<app-page-header [heading]=\"'Forms'\" [icon]=\"'fa-edit'\"></app-page-header>-->\n\n  \n \n    <form [formGroup]=\"rForm\"  (ngSubmit)=\"addPost(rForm.value)\">\n          <fieldset class=\"form-group\">\n              <label>TITLE</label>\n              <input class=\"form-control\" formControlName=\"name\" >\n          </fieldset>\n          <fieldset class=\"form-group\">\n              <label>Description</label>\n              <input class=\"form-control\" formControlName=\"description\" >\n          </fieldset>    \n           <fieldset class=\"form-group\">\n              <label>Stream</label>\n             <select class=\"form-control\" formControlName=\"stream\"    >\n                  <option *ngFor=\"let stream of streams\" [value]=\"stream.streamName\" >{{stream.streamName}}</option>\n              </select>\n            </fieldset>\n            <fieldset class=\"form-group\" >\n              <label>Group</label>\n              <select class=\"form-control\" formControlName=\"group\" >\n                  <option *ngFor=\"let group of groups\" [value]=\"group.groupName\" >{{group.groupName}}</option>\n              </select>\n             \n          </fieldset>\n          <fieldset class=\"form-group\">\n                    <label>Priority</label>\n                    <label class=\"radio-inline\">\n                        <input type=\"radio\" formControlName=\"priority\" id=\"optionsRadiosInline1\" value=\"LOW\" checked=\"\">Low\n                    </label>\n                    <label class=\"radio-inline\">\n                        <input type=\"radio\" formControlName=\"priority\" id=\"optionsRadiosInline2\" value=\"MEDIUM\">Medium\n                    </label>\n                    <label class=\"radio-inline\">\n                        <input type=\"radio\" formControlName=\"priority\" id=\"optionsRadiosInline3\" value=\"HIGH\">High\n                    </label>\n          </fieldset>\n\n          <fieldset class=\"form-group\">\n              <label>StartDate</label>\n              <input class=\"form-control\" formControlName=\"startDate\" >\n          </fieldset>  \n\n          <fieldset class=\"form-group\">\n              <label>EndDate</label>\n              <input class=\"form-control\" formControlName=\"endDate\" >\n          </fieldset>  \n     \n         <!--  <fieldset class=\"form-group\">\n              <label>Start Date</label><input class=\"form-control\">\n              <div class=\"input-group\">\n                <input class=\"form-control\" placeholder=\"yyyy-mm-dd\"  formControlName=\"startDate\" ngbDatepicker #d11=\"ngbDatepicker\">\n                            <div class=\"input-group-addon\" (click)=\"d11.toggle()\" >\n                                <span class=\"fa fa-calendar\"></span>\n                            </div>\n                        </div>\n          </fieldset>\n                    <fieldset class=\"form-group\">\n              <label>Start Date</label><input class=\"form-control\">\n          </fieldset>\n            <fieldset class=\"form-group\">\n              <label>End Date</label><input class=\"form-control\">\n          </fieldset>-->\n         <button type=\"submit\" class=\"btn btn-secondary\" >Submit </button>\n          <button type=\"reset\" class=\"btn btn-secondary\">Reset </button>\n\n    </form>\n  \n\n <!-- /.row -->\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/form/form.component.scss":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".form-container {\n  display: block;\n  width: 90%;\n  padding: 2em;\n  margin: 2em auto;\n  background: #fff; }\n\n.alert {\n  background: #f2edda;\n  padding: 7px;\n  font-size: .9em;\n  margin-bottom: 20px;\n  display: inline-block;\n  -webkit-animation: 2s alertAnim forwards;\n          animation: 2s alertAnim forwards; }\n\n.button {\n  margin-top: 3rem; }\n\nh1 {\n  margin-bottom: 2rem;\n  font-weight: bold;\n  font-family: 'Muli';\n  font-size: 2em; }\n\n@-webkit-keyframes alertAnim {\n  from {\n    opacity: 0;\n    -webkit-transform: translateY(-20px);\n            transform: translateY(-20px); }\n  to {\n    opacity: 1;\n    -webkit-transform: translateY(0);\n            transform: translateY(0); } }\n\n@keyframes alertAnim {\n  from {\n    opacity: 0;\n    -webkit-transform: translateY(-20px);\n            transform: translateY(-20px); }\n  to {\n    opacity: 1;\n    -webkit-transform: translateY(0);\n            transform: translateY(0); } }\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/form/form.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__router_animations__ = __webpack_require__("../../../../../src/app/router.animations.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__("../../../forms/@angular/forms.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__shared_services_user_service__ = __webpack_require__("../../../../../src/app/shared/services/user.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__shared_services_campaigncreation_service__ = __webpack_require__("../../../../../src/app/shared/services/campaigncreation.service.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return FormComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+
+var FormComponent = (function () {
+    function FormComponent(fb, user, campaigncreation) {
+        this.fb = fb;
+        this.user = user;
+        this.campaigncreation = campaigncreation;
+        this.description = '';
+        this.name = '';
+        console.log('User Details>>>>>constructor111');
+        this.rForm = fb.group({
+            'name': [null, __WEBPACK_IMPORTED_MODULE_2__angular_forms__["f" /* Validators */].required],
+            'description': [null, __WEBPACK_IMPORTED_MODULE_2__angular_forms__["f" /* Validators */].compose([__WEBPACK_IMPORTED_MODULE_2__angular_forms__["f" /* Validators */].required, __WEBPACK_IMPORTED_MODULE_2__angular_forms__["f" /* Validators */].minLength(30), __WEBPACK_IMPORTED_MODULE_2__angular_forms__["f" /* Validators */].maxLength(500)])],
+            'group': [],
+            'stream': [],
+            priority: [],
+            startDate: [],
+            endDate: [],
+        });
+    }
+    FormComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        console.log('User Details>>>>>1151');
+        this.userDetails = this.user.getUserDetails().subscribe(function (res) { return _this.userDetails = res.json(); });
+        this.groups = this.campaigncreation.getGroups().subscribe(function (res) { return _this.groups = res.json(); });
+        this.streams = this.campaigncreation.getStreams().subscribe(function (res) { return _this.streams = res.json(); });
+        //this.userDetails=this.user.userData;
+        console.log('User Details>>>>>' + this.userDetails);
+        console.log('Group Id>>>>>' + this.groups);
+    };
+    FormComponent.prototype.saveCampaign = function () {
+        console.log('Submitted....');
+        console.log(this.rForm);
+    };
+    FormComponent.prototype.addPost = function (post) {
+        this.description = post.description;
+        this.name = post.name;
+        console.log('TITLE' + post.name);
+        console.log('Description' + post.description);
+        console.log('group' + post.group);
+        console.log('stream' + post.stream);
+        console.log('JSON' + JSON.stringify(post));
+        console.log('Model' + post);
+        this.campaigncreation.createCampaign(post);
+    };
+    return FormComponent;
+}());
+FormComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+        selector: 'app-form',
+        template: __webpack_require__("../../../../../src/app/layout/form/form.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/layout/form/form.component.scss")],
+        animations: [__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__router_animations__["a" /* routerTransition */])()],
+        providers: [__WEBPACK_IMPORTED_MODULE_4__shared_services_campaigncreation_service__["a" /* CamPaignCreation */], __WEBPACK_IMPORTED_MODULE_3__shared_services_user_service__["a" /* UserService */]]
+    }),
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_2__angular_forms__["e" /* FormBuilder */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__angular_forms__["e" /* FormBuilder */]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_3__shared_services_user_service__["a" /* UserService */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_3__shared_services_user_service__["a" /* UserService */]) === "function" && _b || Object, typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_4__shared_services_campaigncreation_service__["a" /* CamPaignCreation */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_4__shared_services_campaigncreation_service__["a" /* CamPaignCreation */]) === "function" && _c || Object])
+], FormComponent);
+
+var _a, _b, _c;
+//# sourceMappingURL=form.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/form/form.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common__ = __webpack_require__("../../../common/@angular/common.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__("../../../forms/@angular/forms.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__("../../../http/@angular/http.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__form_routing_module__ = __webpack_require__("../../../../../src/app/layout/form/form-routing.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__form_component__ = __webpack_require__("../../../../../src/app/layout/form/form.component.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "FormModule", function() { return FormModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+var FormModule = (function () {
+    function FormModule() {
+    }
+    return FormModule;
+}());
+FormModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1__angular_common__["h" /* CommonModule */],
+            __WEBPACK_IMPORTED_MODULE_4__form_routing_module__["a" /* FormRoutingModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+            __WEBPACK_IMPORTED_MODULE_3__angular_http__["a" /* HttpModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["b" /* ReactiveFormsModule */]
+        ],
+        declarations: [__WEBPACK_IMPORTED_MODULE_5__form_component__["a" /* FormComponent */]]
+    })
+], FormModule);
+
+//# sourceMappingURL=form.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/shared/services/campaigncreation.service.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_http__ = __webpack_require__("../../../http/@angular/http.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_router__ = __webpack_require__("../../../router/@angular/router.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return CamPaignCreation; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var CamPaignCreation = (function () {
+    function CamPaignCreation(http, router) {
+        this.http = http;
+        this.router = router;
+    }
+    CamPaignCreation.prototype.getGroups = function () {
+        // return this.http.get('https://jsonplaceholder.typicode.com/posts');
+        return this.http.get('http://localhost:8080/campaign/allGroup');
+    };
+    CamPaignCreation.prototype.getStreams = function () {
+        //return this.http.get('https://jsonplaceholder.typicode.com/posts');
+        return this.http.get('http://localhost:8080/campaign/allStream');
+    };
+    CamPaignCreation.prototype.createCampaign = function (formData) {
+        var _this = this;
+        console.log('Post:::', formData);
+        //let formData: FormData = new FormData(); 
+        // formData.append('id', model.id); 
+        //formData.append('applicationName', model.applicationName); 
+        this.http.post('http://localhost:8080/campaign/createCampaign', formData)
+            .subscribe(function (res) {
+            ///this.router.navigate(['/blank-page'])
+            return console.log('Response Received', res.json());
+        }, function (error) {
+            return _this.router.navigate(['/blank-page']);
+        }
+        /// console.log("err Received", error)  
+        );
+        console.log('Submitted');
+    };
+    return CamPaignCreation;
+}());
+CamPaignCreation = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])(),
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__angular_http__["b" /* Http */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__angular_http__["b" /* Http */]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_2__angular_router__["a" /* Router */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__angular_router__["a" /* Router */]) === "function" && _b || Object])
+], CamPaignCreation);
+
+var _a, _b;
+//# sourceMappingURL=campaigncreation.service.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=4.chunk.js.map
